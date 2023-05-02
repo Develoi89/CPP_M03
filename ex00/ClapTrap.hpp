8fba08a23bdb0f31f52884ec53576358760c06ce
Eloi Alonso Quiñonero <97552454+Develoi89@users.noreply.github.com>
2023-05-02 17:13:48 +0200

    ex00 started

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+    private:
+        std::string _Name;
+        unsigned int _hp;
+        unsigned int _ep;
+        unsigned int _ad;
+    public:
+        
+};
+
+
+
+#endif
